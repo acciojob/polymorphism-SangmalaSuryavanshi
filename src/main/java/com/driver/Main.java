363ccb3608c27f86cddff3fed7f99e4bfb39d8b9
SangmalaSuryavanshi <161364128+SangmalaSuryavanshi@users.noreply.github.com>
2024-06-17 23:28:32 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,16 +5,16 @@ public class Main {
     System.out.println(p.product(10, 20));
     System.out.println(p.product(10,20,30));
     System.out.println(p.product(10.5,20.5));
-class Product{
+public static class Product{
     public int product(int x, int y) {
-        return x+y;
+        return x*y;
     }
     public int product(int x, int y, int z) {
-        return x+y+z;
+        return x*y*z;
     }
     public double product(double x, double y) {
-        return(x+y);
+        return(x*y);
     }
 
-}
+    }
 }
