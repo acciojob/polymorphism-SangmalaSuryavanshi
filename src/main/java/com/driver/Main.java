9cfb70949330e1d25484560501a89f2413dd216e
SangmalaSuryavanshi <161364128+SangmalaSuryavanshi@users.noreply.github.com>
2024-06-18 00:15:11 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[]){
     Product p = new Product();
     System.out.println(p.product(10, 20));
     System.out.println(p.product(10,20,30));
     System.out.println(p.product(10.5,20.5));
+    }
 class Product{
     public int product(int x, int y) {
         return x*y;
